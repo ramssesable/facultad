9f45d9fce80888a813ddf9c8d2148ddb10b84756
ramssesable <ramssesable13@gmail.com>
2015-08-14 18:21:30 -0700

    14 de agosto
    cosillas

--- SSI/solicitudes_procesa.php
+++ SSI/solicitudes_procesa.php
@@ -7,50 +7,25 @@
 
 	$sql 		= 'SELECT usuario,correo,telefono,dependencia,asunto,descripcion,estatus FROM solicitudes WHERE usuario=11111';
 	$result 	= mysql_query($sql);
-	
-	$bandera = 0;
-	while($arreglo 	= mysql_fetch_array($result)) {
-		if (!$bandera == 1) {
-			$bandera = 1;
-			$arreglo_solicitudes=array("result" => $bandera,"data" => array())
+	if ( !mysql_fetch_array($result) > 0)
+		$arreglo_solicitudes = array("result" => 0);
+	else {
+		$arreglo_solicitudes = array("result" => 1); 
+		$data = array();
+
+		while($arreglo 	= mysql_fetch_array($result)) {
+			$fila = array();
+			for ($i = 1; $i <= mysql_num_fields($result); $i ++) {
+				array_push($fila, $arreglo[$i]);
+			}
+			array_push($data, $fila);
+			echo "$arreglo[0] $arreglo[1] $arreglo[2] $arreglo[3] $arreglo[4] $arreglo[5] $arreglo[6] <br>";
 		}
+	}
 		
-		echo "$arreglo[0] $arreglo[1] $arreglo[2] $arreglo[3] $arreglo[4] $arreglo[5] $arreglo[6] <br>";
-	}
-
-	if (!$bandera == 1) {
-
-	}
-
-	
-
 	/*if ($arreglo > 0) {
 
-		$bolRegresa = odbc_exec($conect, $sql);
-
-		$numFila = 0;
-		while(odbc_fetch_row($bolRegresa)){
-			$arrDatoColumna = array();
-			$arrNombreCampos = array();
-			for ($i = 1; $i <= odbc_num_fields($bolRegresa); $i ++){
-			      if ($numFila == 0) {
-					 // pone los nombres de columnas (campos) del arreglo una por una
-			         array_push($arrNombreCampos, odbc_field_name($bolRegresa, $i));
-				  }
-				     // Pone los valores de cada columna 
-				     array_push($arrDatoColumna, odbc_result($bolRegresa, $i));
-			}
-			// Solo lo hace una vez, es el nombre total de las columnas de la tabla
-			if ($numFila == 0) {
-			 array_push($this->arrResultado, $arrNombreCampos );   
-			}
-
-			// Contador de fila del arreglo
-			$numFila++;
-
-			// Inserta la filacompleta  al arreglo arrResultado
-			array_push($this->arrResultado, $arrDatoColumna); 
-		}
+		
 
 		//$arreglo_solicitudes=array("result" => 1,"data" => array(array("nombre" => "Ramon", "edad" => 25),array("nombre" => "Ramsses", "edad" => 23)));
 		//header('Content-Type:text/json');
@@ -59,31 +34,5 @@
 		echo "Le falto mas caldo";
 	}*/
 
-	/************
-	
-	$arrResultado = array();
-	$numFila = 0;
-	while(odbc_fetch_row($bolRegresa)){
-	  $arrDatoColumna = array();
-	  $arrNombreCampos = array();
-	  for ($i = 1; $i <= odbc_num_fields($bolRegresa); $i ++){
-		      if ($numFila == 0) {
-				 // pone los nombres de columnas (campos) del arreglo una por una
-		         array_push($arrNombreCampos, odbc_field_name($bolRegresa, $i));
-			  }
-			     // Pone los valores de cada columna 
-			     array_push($arrDatoColumna, odbc_result($bolRegresa, $i));
-	 }
-	  // Solo lo hace una vez, es el nombre total de las columnas de la tabla
-	  if ($numFila == 0) {
-		 array_push($this->arrResultado, $arrNombreCampos );   
-	  }
-	  
-	  // Contador de fila del arreglo
-	 $numFila++;
-	 
-	 // Inserta la filacompleta  al arreglo arrResultado
-	 array_push($this->arrResultado, $arrDatoColumna); 
-	
-	*************/
+
 ?>
